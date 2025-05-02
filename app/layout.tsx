0e9843aa172c0ba6beb7826c020c3fc3a97bf3ee
Clóvis Neto <clovisdasilvaneto@gmail.com>
2025-05-02 23:44:01 +0200

    feat: add openGraph info

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,14 @@ export const metadata: Metadata = {
   title: "NevTec",
   description:
     "Desenvolvimento de software, aplicativos, sites e automações com inteligência artificial",
+  openGraph: {
+    type: "website",
+    url: "https://nevtec.netlify.app/",
+    title: "NevTec",
+    description:
+      "Desenvolvimento de software, aplicativos, sites e automações com inteligência artificial",
+    siteName: "NevTec",
+  },
 };
 
 export default function RootLayout({
